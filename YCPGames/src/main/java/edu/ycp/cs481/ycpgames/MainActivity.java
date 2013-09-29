461e2592016356a2314545a7b038f755882e82e4
Brian <brian.michael.martin@gmail.com>
2013-09-29 15:59:51 -0400

    Linked buttons in layout to java program. Added listeners. Added placeholder code in for button press

--- YCPGames/src/main/java/edu/ycp/cs481/ycpgames/MainActivity.java
+++ YCPGames/src/main/java/edu/ycp/cs481/ycpgames/MainActivity.java
@@ -3,13 +3,41 @@ package edu.ycp.cs481.ycpgames;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
-
+    //declare instance of buttons
+    private ImageButton TicTacToeButton;
+    private ImageButton DotsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Get reference to TicTacToeButton
+        TicTacToeButton = (ImageButton)findViewById(R.id.TicTacToeButton);
+        //Set listener for TicTacToeButton
+        TicTacToeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //When TicTacToe button is pressed:
+                //Placeholder "Toast" till TicTacToe is implemented
+                Toast.makeText(MainActivity.this, R.string.TicTacToe, Toast.LENGTH_SHORT).show();
+            }
+        });
+        //Get reference to DotsButton
+        DotsButton = (ImageButton)findViewById(R.id.DotsButton);
+        //Set listener for DotsButton
+        DotsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //When dots button is pressed:
+                //Placeholder "Toast" till Dots is implemented
+                Toast.makeText(MainActivity.this, R.string.Dots, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
@@ -19,5 +47,5 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
 }
