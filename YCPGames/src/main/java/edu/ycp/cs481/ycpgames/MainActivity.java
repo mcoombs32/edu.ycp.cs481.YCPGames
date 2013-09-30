6a72cc7f09db3bb2e14acfa2f3a4a15f4218e384
Brian <brian.michael.martin@gmail.com>
2013-09-29 23:13:45 -0400

    Adding debug info and overiding lifecycle methods

--- YCPGames/src/main/java/edu/ycp/cs481/ycpgames/MainActivity.java
+++ YCPGames/src/main/java/edu/ycp/cs481/ycpgames/MainActivity.java
@@ -2,6 +2,7 @@ package edu.ycp.cs481.ycpgames;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
@@ -9,15 +10,19 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+    //define debug string
+    private static final String TAG = "YCPGamesActivity";
     //declare instance of buttons
     protected ImageButton TicTacToeButton;
     protected ImageButton DotsButton;
     protected ImageButton CheckersButton;
     protected ImageButton SettingsButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate() called");
 
         //Get references to main menu buttons
         TicTacToeButton = (ImageButton)findViewById(R.id.TicTacToeButton);
@@ -29,6 +34,7 @@ public class MainActivity extends Activity {
         TicTacToeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d(TAG, "TicTacToeButton Pressed");
                 //When TicTacToe button is pressed:
                 //Placeholder "Toast" till TicTacToe is implemented
                 Toast.makeText(MainActivity.this, R.string.TicTacToe, Toast.LENGTH_SHORT).show();
@@ -38,6 +44,7 @@ public class MainActivity extends Activity {
         DotsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d(TAG, "DotsButton Pressed");
                 //When dots button is pressed:
                 //Placeholder "Toast" till Dots is implemented
                 Toast.makeText(MainActivity.this, R.string.Dots, Toast.LENGTH_SHORT).show();
@@ -47,6 +54,7 @@ public class MainActivity extends Activity {
         CheckersButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d(TAG, "CheckersButton Pressed");
                 //When Checkers button is pressed:
                 //Placeholder "Toast" till Checkers is implemented
                 Toast.makeText(MainActivity.this, R.string.Checkers, Toast.LENGTH_SHORT).show();
@@ -56,8 +64,9 @@ public class MainActivity extends Activity {
         SettingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //When Checkers button is pressed:
-                //Placeholder "Toast" till Checkers is implemented
+                Log.d(TAG, "SettingsButton Pressed");
+                //When Settings button is pressed:
+                //Placeholder "Toast" till Settings is implemented
                 Toast.makeText(MainActivity.this, R.string.Settings, Toast.LENGTH_SHORT).show();
                 //TODO: link to settings
             }
@@ -66,6 +75,35 @@ public class MainActivity extends Activity {
 
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
